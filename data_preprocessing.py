c7910388b34a77ba3b8038dc5287fe544ccd4244
Garstka <jakub.garstka@fujitsu.com>
2021-11-24 18:34:46 +0100

    Start working on data pre-processing

---
+++ data_preprocessing.py
@@ -0,0 +1,10 @@
+from load_data import get_data
+import os
+import numpy as np
+
+#Fetch data
+train_data = get_data('C:\\Workspace_studies\\Project_main\\Input\\train')
+val_data = get_data('C:\\Workspace_studies\\Project_main\\Input\\val')
+test_data = get_data('C:\\Workspace_studies\\Project_main\\Input\\test')
+
+# to be done ...
